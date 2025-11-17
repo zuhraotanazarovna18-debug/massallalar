d3249c0090e7ec9eae2bf4d5041003315c68155a
zuhra10 <zuhraotanazarovna18@gmail.com>
2025-11-17 18:21:24 +0500

    2 TOPSHIRIQ YAKUNLANDI

---
+++ masalalar.py
@@ -0,0 +1,7 @@
+ismlar = ["XUSNORA","O'zim","DILNURA","S.Marjona","Maftuna","Rayhona","R.Ruxshona","Og'abek","Muhammad", "Samadjon", "Sanjar"]
+
+for ism in ismlar:
+    if ism == 'DILNURA':
+        print(f"{ism} - nichiksan ")
+    else:
+        print("asalomu aleykum")
